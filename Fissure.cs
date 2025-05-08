7051120134df9b0e8dfe8172d14030337c6cbb5f
Ebon1 <73665110+Ebon1@users.noreply.github.com>
2025-05-08 20:11:41 +0200

    Fissure mod KILLS calamity

---
+++ Fissure.cs
@@ -0,0 +1,23 @@
+global using Color = Microsoft.Xna.Framework.Color;
+
+using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Content;
+using Terraria.ModLoader;
+
+namespace Fissure
+{
+    public class Fissure : Mod
+    {
+        public static Fissure Instance => ModContent.GetInstance<Fissure>();
+        public Fissure() => MusicSkipsVolumeRemap = true;
+        public override void Load()
+        {
+            LoadEffects();
+        }
+        public static void LoadEffects()
+        {
+            static Asset<Effect> LoadEffect(string path) => ModContent.Request<Effect>("Fissure/Effects/" + path);
+            // LoadEffect("whatever"); yeahhh
+        }
+    }
+}
